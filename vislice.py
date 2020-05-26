a5a187d7654a7a10960d3bf73266d79af2f07790
Meta Trdin <meta.trdin5@gmail.com>
2020-05-26 16:55:51 +0200

    dodali piškotke

--- vislice.py
+++ vislice.py
@@ -1,36 +1,54 @@
 import bottle
 import model
 
-vislice = model.Vislice()
+#konstanti
+DATOTEKA_S_STANJEM = "stanje.json"
+
+SECRET = "VISLICE"
+
 
 bottle.TEMPLATE_PATH.insert(0, "views")
+
+vislice = model.Vislice(DATOTEKA_S_STANJEM)
 
 @bottle.get("/")
 def index():
     #prva stran
-    return bottle.template("index.tpl")
+    return bottle.template("index")
 
 @bottle.get("/img/<picture>")
 def static_file(picture):
     return bottle.static_file(picture, "img")
 
-@bottle.post("/igra/")
+@bottle.post("/nova_igra/")
 def nova_igra():
-    #naredi novo igro
+    vislice.nalozi_igre_iz_datoteke()
     id_igre = vislice.nova_igra()
     #preusmeri na naslov za igranje nove igre
-    bottle.redirect(f"/igra/{id_igre}/")
+    vislice.zapisi_igre_v_datoteko()
+    bottle.response.set_cookie("id_igre", id_igre, path="/", secret="SECRET")
+    bottle.redirect("/igra/")
 
-@bottle.get("/igra/<id_igre:int>/")
-def pokazi_igro(id_igre):
+
+@bottle.get("/igra/")
+def pokazi_igro():
+    vislice.nalozi_igre_iz_datoteke()
+    id_igre = bottle.request.get_cookie("id_igre", secret=SECRET)
     (igra, stanje) = vislice.igre[id_igre]
     return bottle.template("igra.tpl", igra=igra, stanje=stanje, id_igre=id_igre, ZMAGA=model.ZMAGA, PORAZ=model.PORAZ)
 
-@bottle.post("/igra/<id_igre:int>/")
-def ugibaj(id_igre):
-    crka = bottle.request.forms.get("crka")
+@bottle.post("/igra/")
+def ugibaj():
+    vislice.nalozi_igre_iz_datoteke()
+    id_igre = bottle.request.get_cookie("id_igre", secret=SECRET)
+    crka = bottle.request.forms.crka #ce das get zgubis šumnike
+
     vislice.ugibaj(id_igre, crka)
-    bottle.redirect(f"/igra/{id_igre}/")
-    #neki je narobe
+    vislice.zapisi_igre_v_datoteko()
+
+    bottle.redirect(f"/igra/")
+   
 
 bottle.run(debug=True, reloader=True)
+
+#neki je narobe
